68d13e516180f0ed50c253e449f9ed7012e70fb1
cort-robinson <cortrobinson99@gmail.com>
2021-01-19 16:16:41 +0000

    fix import/syntax error

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -2,12 +2,13 @@
 """Starts a Flask web application"""
 from flask import Flask, render_template
 from models import storage
+from models.state import State
 app = Flask(__name__)
 
 
 @app.route('/states_list', strict_slashes=False)
 def states_list():
-    states = storage.all('State').values()
+    states = storage.all(State).values()
     return render_template('7-states_list.html', states=states)
 
 
